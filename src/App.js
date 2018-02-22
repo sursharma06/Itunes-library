9177839d520a45338ef4cc6af107c02f484c44bd
Surbhi Sharma <sharma.surbhi13@gmail.com>
2018-02-21 19:37:27 -0500

    Add constructor func with fetch to App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,36 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      albums: [],
+    };
+    this.artistSearch('adele');
+  }
+
+  artistSearch = (ARTIST_NAME) => {
+    fetch(`https://itunes.apple.com/search?term=${ARTIST_NAME}`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            albums: result,
+          });
+        },
+
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        }
+      );
+  };
+
   render() {
     return (
       <div className="App">
